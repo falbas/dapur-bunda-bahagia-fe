825ea8107dbc53574a16e4303fb32392840225de
falbas <farhanalbas30@gmail.com>
2024-01-01 21:43:55 +0700

    update: product category filter

--- src/app/(user)/layout.js
+++ src/app/(user)/layout.js
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar'
 export default function UserLayout({ children }) {
   return (
     <div className="relative max-w-md h-[100svh] mx-auto bg-white">
-      <div className="relative pt-8 h-full overflow-auto no-scrollbar pb-20 px-4">
+      <div className="relative pt-20 h-full overflow-auto no-scrollbar pb-20 px-4">
         {children}
       </div>
 
--- src/app/(user)/page.js
+++ src/app/(user)/page.js
@@ -1,13 +1,23 @@
 'use client'
 
 import useSWR from 'swr'
-import { fetcherSWR } from '@/helpers/fetcher'
+import { fetcherSWR, fetcher } from '@/helpers/fetcher'
 import { currencyFormat } from '@/helpers/utils'
-import { Button } from '@mantine/core'
+import { Button, Select } from '@mantine/core'
 import { addToCart } from '@/helpers/cartManager'
+import { useState, useEffect } from 'react'
 
 export default function Page() {
-  const { data, isLoading, error } = useSWR('/products', fetcherSWR)
+  const [category, setCategory] = useState('0')
+  const { data, isLoading, error, mutate } = useSWR(
+    category === '0' ? '/products' : `/products?category=${category}`,
+    fetcherSWR,
+  )
+  const { data: categories } = useSWR('/categories', fetcherSWR)
+
+  useEffect(() => {
+    mutate()
+  }, [category])
 
   const handleAddToCart = (item) => {
     addToCart({
@@ -26,28 +36,51 @@ export default function Page() {
       ) : error ? (
         'Error'
       ) : (
-        <div className="grid grid-cols-2 gap-4">
-          {data?.data.map((item, key) => (
-            <div key={key}>
-              <div className="w-full h-40 mb-2">
-                <img
-                  src={item.image_url}
-                  className="w-full h-full object-cover rounded-xl"
-                />
+        <div>
+          <div className="fixed -ml-4 px-4 py-4 w-full max-w-md top-0 z-10 bg-white">
+            <Select
+              placeholder="Pilih kategori"
+              value={category}
+              onChange={setCategory}
+              allowDeselect={false}
+              data={
+                [
+                  {
+                    value: '0',
+                    label: 'Semua',
+                  },
+                  ...categories?.data.map((item) => ({
+                    value: String(item.id),
+                    label: item.name,
+                  })),
+                ] ?? []
+              }
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            {data?.data.map((item, key) => (
+              <div key={key}>
+                <div className="w-full h-40 mb-2">
+                  <img
+                    src={item.image_url}
+                    className="w-full h-full object-cover rounded-xl"
+                  />
+                </div>
+                <h1 className="font-bold">{item.name}</h1>
+                <h2>{currencyFormat(item.price)}</h2>
+                <Button
+                  onClick={() => handleAddToCart(item)}
+                  variant="outline"
+                  color="teal"
+                  radius="xl"
+                  fullWidth
+                  disabled={!item.status}
+                >
+                  {item.status ? 'Tambah' : 'Tidak Tersedia'}
+                </Button>
               </div>
-              <h1 className="font-bold">{item.name}</h1>
-              <h2>{currencyFormat(item.price)}</h2>
-              <Button
-                onClick={() => handleAddToCart(item)}
-                variant="outline"
-                color="teal"
-                radius="xl"
-                fullWidth
-              >
-                Tambah
-              </Button>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       )}
     </>
